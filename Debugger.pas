Hendi48/Magicmida

commit 730915c9bf6aa9a3f6766630cace68f4a8733173

Don't apply API breakpoints when using ScyllaHide

Debugger.pas
@@ -327,31 +327,33 @@ begin
   CloseHandleAPI := GetProcAddress(GetModuleHandle(kernel32), 'CloseHandle');
   FHW1.Address := Cardinal(CloseHandleAPI);
 
-  NtSIT := GetProcAddress(GetModuleHandle('ntdll.dll'), 'ZwSetInformationThread');
-  FHW3.Address := Cardinal(NtSIT);
-  KiFastSystemCall := GetProcAddress(GetModuleHandle('ntdll.dll'), 'KiFastSystemCall');
-
-  if not (IsWow64Process(FProcess.hProcess, FWow64) and FWow64) then
-  begin
-    VirtualProtectEx(FProcess.hProcess, KiFastSystemCall, 1, PAGE_EXECUTE_READWRITE, @x);
-    Buf := $CC;
-    WriteProcessMemory(FProcess.hProcess, KiFastSystemCall, @Buf, 1, x);
-    FSoftBPs.Add(KiFastSystemCall, $8B);
-    NtQIP := PCardinal(Cardinal(GetProcAddress(GetModuleHandle('ntdll.dll'), 'ZwQueryInformationProcess')) + 1)^;
-  end
-  else
-  begin
-    NtQIP64 := GetProcAddress(GetModuleHandle('ntdll.dll'), 'ZwQueryInformationProcess');
-    FHW4.Address := Cardinal(NtQIP64);
-  end;
-
-  UpdateDR(DebugEv.CreateProcessInfo.hThread);
-
   if FileExists('InjectorCLIx86.exe') then
   begin
     Log(ltGood, 'Applying ScyllaHide');
     ShellExecute(0, 'open', 'InjectorCLIx86.exe', PChar(Format('pid:%d %s nowait', [FProcess.dwProcessId, ExtractFilePath(ParamStr(0)) + 'HookLibraryx86.dll'])), nil, SW_HIDE);
+  end
+  else
+  begin
+    NtSIT := GetProcAddress(GetModuleHandle('ntdll.dll'), 'ZwSetInformationThread');
+    FHW3.Address := Cardinal(NtSIT);
+    KiFastSystemCall := GetProcAddress(GetModuleHandle('ntdll.dll'), 'KiFastSystemCall');
+
+    if not (IsWow64Process(FProcess.hProcess, FWow64) and FWow64) then
+    begin
+      VirtualProtectEx(FProcess.hProcess, KiFastSystemCall, 1, PAGE_EXECUTE_READWRITE, @x);
+      Buf := $CC;
+      WriteProcessMemory(FProcess.hProcess, KiFastSystemCall, @Buf, 1, x);
+      FSoftBPs.Add(KiFastSystemCall, $8B);
+      NtQIP := PCardinal(Cardinal(GetProcAddress(GetModuleHandle('ntdll.dll'), 'ZwQueryInformationProcess')) + 1)^;
+    end
+    else
+    begin
+      NtQIP64 := GetProcAddress(GetModuleHandle('ntdll.dll'), 'ZwQueryInformationProcess');
+      FHW4.Address := Cardinal(NtQIP64);
+    end;
   end;
+
+  UpdateDR(DebugEv.CreateProcessInfo.hThread);
 
   //FetchMemoryRegions;
   TMInit(DebugEv.CreateProcessInfo.hFile);

commit 8cf2d0636239d63635cf5d4f20f809863c10f128

Fix bug introduced by 730915c: When no HWBP is currently enabled, bit 14 in Dr6 is always 0

Debugger.pas
@@ -506,7 +506,8 @@ begin
   end
   else
   begin
-    if ((C.Dr6 shr 14) and 1) = 0 then // Bit 14: Single-step execution mode
+    // Check if Single-step execution mode (bit 14)
+    if (((C.Dr6 shr 14) and 1) = 0) and (FHW1.IsSet or FHW2.IsSet or FHW3.IsSet or FHW4.IsSet) then
     begin
       BPA := 0;
       case C.Dr6 and $F of